8c884afef3a7f9f2a648a248ca15487ec2cb555b
Harshvardhan Karn <harshvardhan@deepfence.io>
2022-05-10 09:01:53 +0000

    change ingest url

--- internal/deepfence/client.go
+++ internal/deepfence/client.go
@@ -97,8 +97,9 @@ func (c *Client) SendScanStatustoConsole(vulnerabilityScanMsg string, status str
 	vulnerabilityScanMsg = strings.Replace(vulnerabilityScanMsg, "\n", " ", -1)
 	scanLog := fmt.Sprintf("{\"scan_id\":\"%s\",\"time_stamp\":%d,\"cve_scan_message\":\"%s\",\"action\":\"%s\",\"type\":\"cve-scan\",\"node_type\":\"%s\",\"node_id\":\"%s\",\"scan_type\":\"%s\",\"host_name\":\"%s\",\"host\":\"%s\",\"kubernetes_cluster_name\":\"%s\"}", c.config.ScanId, util.GetIntTimestamp(), vulnerabilityScanMsg, status, c.config.NodeType, c.config.NodeId, c.config.ScanType, c.config.HostName, c.config.HostName, c.config.KubernetesClusterName)
 	postReader := bytes.NewReader([]byte(scanLog))
-	ingestScanStatusAPI := fmt.Sprintf("https://" + c.mgmtConsoleUrl + "/df-api/ingest?doc_type=" + cveScanLogsIndexName)
-	_, err := c.HttpRequest(MethodPost, ingestScanStatusAPI, postReader, nil)
+	ingestScanStatusAPI := fmt.Sprintf("https://" + c.mgmtConsoleUrl + "/ingest/topics/" + cveScanLogsIndexName)
+
+	_, err := c.HttpRequest(MethodPost, ingestScanStatusAPI, postReader, nil, "application/vnd.kafka.json.v2+json")
 	return err
 }
 
@@ -118,7 +119,7 @@ func (c *Client) GetApiAccessToken() (string, error) {
 	resp, err := c.HttpRequest(MethodPost,
 		"https://"+c.mgmtConsoleUrl+"/deepfence/v1.5/users/auth",
 		bytes.NewReader([]byte(`{"api_key":"`+c.config.DeepfenceKey+`"}`)),
-		nil)
+		nil, "")
 	if err != nil {
 		return "", err
 	}
@@ -136,7 +137,7 @@ func (c *Client) GetApiAccessToken() (string, error) {
 func (c *Client) getVulnerabilityScanStatus() (string, error) {
 	resp, err := c.HttpRequest(MethodGet,
 		"https://"+c.mgmtConsoleUrl+"/deepfence/v1.5/cve-scan/"+url.PathEscape(c.config.NodeId),
-		nil, map[string]string{"Authorization": "Bearer " + c.getApiAccessToken()})
+		nil, map[string]string{"Authorization": "Bearer " + c.getApiAccessToken()}, "")
 	if err != nil {
 		return "", err
 	}
@@ -174,7 +175,7 @@ func (c *Client) GetVulnerabilityScanSummary() (*VulnerabilityScanDetail, error)
 	resp, err := c.HttpRequest(MethodPost,
 		"https://"+c.mgmtConsoleUrl+"/deepfence/v1.5/vulnerabilities/image_report?lucene_query=&number=30&time_unit=day",
 		bytes.NewReader([]byte(`{"filters":{"cve_container_image":"`+c.config.NodeId+`","scan_id":"`+c.config.ScanId+`"}}`)),
-		map[string]string{"Authorization": "Bearer " + c.getApiAccessToken()})
+		map[string]string{"Authorization": "Bearer " + c.getApiAccessToken()}, "")
 	if err != nil {
 		return nil, err
 	}
@@ -208,7 +209,7 @@ func (c *Client) GetVulnerabilities() (*Vulnerabilities, error) {
 		resp, err = c.HttpRequest(MethodPost,
 			fmt.Sprintf("https://%s/deepfence/v1.5/search?from=%d&size=%d&lucene_query=&number=1&time_unit=hour", c.mgmtConsoleUrl, from, pageSize),
 			bytes.NewReader([]byte(`{"_type":"cve","_source":[],"filters":{"masked":"false","type":["cve"],"cve_container_image":"`+c.config.NodeId+`","scan_id":"`+c.config.ScanId+`"},"node_filters":{}}`)),
-			map[string]string{"Authorization": "Bearer " + c.getApiAccessToken()})
+			map[string]string{"Authorization": "Bearer " + c.getApiAccessToken()}, "")
 		var vuln Vulnerabilities
 		err = json.Unmarshal(resp, &vuln)
 		if err != nil || vuln.Success == false || len(vuln.Data.Hits) == 0 {
@@ -239,7 +240,7 @@ func (c *Client) SendSBOMtoConsole(sbom []byte) error {
 	urlValues.Set("scan_type", c.config.ScanType)
 	urlValues.Set("container_name", c.config.ContainerName)
 	requestUrl := fmt.Sprintf("https://"+c.mgmtConsoleUrl+"/vulnerability-mapper-api/vulnerability-scan?%s", urlValues.Encode())
-	_, err := c.HttpRequest(MethodPost, requestUrl, bytes.NewReader(sbom), nil)
+	_, err := c.HttpRequest(MethodPost, requestUrl, bytes.NewReader(sbom), nil, "")
 	return err
 }
 
@@ -273,8 +274,9 @@ func (c *Client) SendSBOMtoES(sbom []byte) error {
 		return err
 	}
 	postReader := bytes.NewReader(docBytes)
-	ingestScanStatusAPI := fmt.Sprintf("https://" + c.mgmtConsoleUrl + "/df-api/ingest?doc_type=" + sbomCveScanLogsIndexName)
-	_, err = c.HttpRequest("POST", ingestScanStatusAPI, postReader, nil)
+	ingestScanStatusAPI := fmt.Sprintf("https://" + c.mgmtConsoleUrl + "/ingest/topics/" + sbomCveScanLogsIndexName)
+
+	_, err = c.HttpRequest("POST", ingestScanStatusAPI, postReader, nil, "application/vnd.kafka.json.v2+json")
 	if err != nil {
 		return err
 	}
@@ -307,15 +309,15 @@ func (c *Client) sendSBOMArtifactsToES(artifacts []Artifact) error {
 		return err
 	}
 	postReader := bytes.NewReader(docBytes)
-	ingestScanStatusAPI := fmt.Sprintf("https://" + c.mgmtConsoleUrl + "/df-api/ingest?doc_type=" + sbomArtifactsIndexName)
-	_, err = c.HttpRequest("POST", ingestScanStatusAPI, postReader, nil)
+	ingestScanStatusAPI := fmt.Sprintf("https://" + c.mgmtConsoleUrl + "/ingest/topics" + sbomArtifactsIndexName)
+	_, err = c.HttpRequest("POST", ingestScanStatusAPI, postReader, nil, "application/vnd.kafka.json.v2+json")
 	if err != nil {
 		return err
 	}
 	return nil
 }
 
-func (c *Client) HttpRequest(method string, requestUrl string, postReader io.Reader, header map[string]string) ([]byte, error) {
+func (c *Client) HttpRequest(method string, requestUrl string, postReader io.Reader, header map[string]string, contentType string) ([]byte, error) {
 	retryCount := 0
 	var response []byte
 	for {
@@ -325,7 +327,12 @@ func (c *Client) HttpRequest(method string, requestUrl string, postReader io.Rea
 		}
 		httpReq.Close = true
 		httpReq.Header.Add("deepfence-key", c.config.DeepfenceKey)
-		httpReq.Header.Set("Content-Type", "application/json")
+		if contentType == "" {
+			httpReq.Header.Set("Content-Type", "application/json")
+		} else {
+			httpReq.Header.Set("Content-Type", contentType)
+		}
+
 		if header != nil {
 			for k, v := range header {
 				httpReq.Header.Add(k, v)
