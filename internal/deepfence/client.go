587fb105396f88d2d0ef5ee19fce80dc93448dbd
Mukul Varshney <72725782+mukuldeepfence@users.noreply.github.com>
2023-01-19 11:17:52 +0530

    Adding image name (#36)
    * adding image name

--- internal/deepfence/client.go
+++ internal/deepfence/client.go
@@ -276,6 +276,7 @@ func (c *Client) SendSBOMtoConsole(sbom []byte) error {
 func (c *Client) SendSBOMtoES(sbom []byte) error {
 	var sbomDoc = make(map[string]interface{})
 	sbomDoc["scan_id"] = c.config.ScanId
+	sbomDoc["image_name"] = c.config.Source
 	sbomDoc["node_id"] = c.config.NodeId
 	sbomDoc["scan_type"] = c.config.ScanType
 	sbomDoc["node_type"] = c.config.NodeType
