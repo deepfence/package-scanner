e4a13cf09758f77e1993d4cecebf2c777eca6c4c
Mukul Varshney <72725782+mukuldeepfence@users.noreply.github.com>
2023-01-20 20:17:47 +0530

    minor change

--- internal/deepfence/client.go
+++ internal/deepfence/client.go
@@ -262,7 +262,7 @@ func (c *Client) SendSBOMtoConsole(sbom []byte) error {
 	urlValues.Set("kubernetes_cluster_name", c.config.KubernetesClusterName)
 	urlValues.Set("host_name", c.config.HostName)
 	nodeId := c.config.NodeId
-	if c.config.RegistryId == "" {
+	if c.config.RegistryId != "" {
 		log.Error("it is coming to the condition")
 		nodeId = c.config.ImageName
 		log.Error(nodeId)
@@ -274,7 +274,7 @@ func (c *Client) SendSBOMtoConsole(sbom []byte) error {
 	urlValues.Set("scan_type", c.config.ScanType)
 	urlValues.Set("container_name", c.config.ContainerName)
 	log.Errorf("sbom to console %+v", c.config)
-	fmt.Printf("%+v",c.config)
+	log.Errorf("urlvalues %+v", urlValues)
 	requestUrl := fmt.Sprintf("https://"+c.mgmtConsoleUrl+"/vulnerability-mapper-api/vulnerability-scan?%s", urlValues.Encode())
 	_, err := c.HttpRequest(MethodPost, requestUrl, bytes.NewReader(sbom), nil, "")
 	if err != nil {
