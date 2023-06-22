d7bdf74fa56c0b0165bf25ef70f9d7f8445cced2
gnmahanth <mahanth@deepfence.io>
2023-06-22 11:20:04 +0000

    update syft and grype

--- tools/syft-bin/get_syft.sh
+++ tools/syft-bin/get_syft.sh
@@ -4,7 +4,7 @@ set -x -e
 
 rm -rf syft*
 # git clone https://github.com/anchore/syft && cd syft && git checkout 1d14f22e4538f03a1896b2d4e1d99a65e52b6f30
-git clone https://github.com/deepfence/syft.git && cd syft && git checkout optimise-resolver
+git clone https://github.com/deepfence/syft.git && cd syft && git checkout optimise-resolver-1
 cd cmd/syft
 export CGO_ENABLED=0
 GOOS=linux GOARCH=amd64 go build -v -o syft_linux_amd64 .
